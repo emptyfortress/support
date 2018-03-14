9340a8da299692e603cec326c215e7bd8e5d90a0
Volkov Alexander <volkov.525@gmail.com>
2018-03-14 10:53:38 +0300

    Initial commit

---
+++ layouts/dark.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="dark">
+		<nuxt/>
+	</div>
+</template>
+
+<style lang="scss">
+.dark {
+	position: absolute;
+	top: 0;
+	left: 0;
+	min-height: 100%;
+	width: 100%;
+	background: #333;
+	color: #fff;
+	a {
+		color: #fff;
+	}
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+	<section class="container">
+		<div>
+				<h1 class="title">support</h1>
+				<h2 class="subtitle"> DV support site </h2>
+
+				<div class="links"> 
+					<nuxt-link to="/test">test</nuxt-link>
+				</div>
+		</div>
+	</section>
+</template>
+
+<script>
+
+</script>
+
+<style lang = "scss">
+.container {
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-align: center;
+}
+
+.title {
+	font-family: "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+	display: block;
+	font-weight: 300;
+	font-size: 100px;
+	color: #35495e;
+	/* color: #f00; */
+	letter-spacing: 1px;
+}
+
+.subtitle {
+	font-weight: 300;
+	font-size: 42px;
+	color: #526488;
+	word-spacing: 5px;
+	padding-bottom: 15px;
+}
+
+.links {
+	/* padding-top: 15px; */
+}
+</style>
