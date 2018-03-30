75a84a42e8b31aca55aab9922a57cb0a38efbe92
Volkov Alexander <volkov.525@gmail.com>
2018-03-30 10:47:33 +0300

    fix links on docs page - all lead to platforma

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -33,8 +33,8 @@ export default {
 				{ 
 					id: 2, expand: false, name: 'Web-client',
 					childs: [
-						{ id: 1, to: '', name: 'Web-client, версия 9' },
-						{ id: 1, to: '', name: 'Web-client, версия 10' },
+						{ id: 1, to: '/docs/platform/1', name: 'Web-client, версия 9' },
+						{ id: 1, to: '/docs/platform/2', name: 'Web-client, версия 10' },
 					]
 				},
 				{
@@ -50,25 +50,25 @@ export default {
 				},
 				{ id: 5, expand: false, name: 'Приложение «Договоры»',
 					childs: [
-						{ id: 1, to: '', name: 'Приложение «Договоры» v.5.4.3' },
+						{ id: 1, to: '/docs/platform/1', name: 'Приложение «Договоры» v.5.4.3' },
 					]
 				},
 				{ id: 6, expand: false, name: 'Приложение «Почтовый клиент»',
 					childs: [
-						{ id: 1, to: '', name: 'Модуль «Почтовый клиент»' },
+						{ id: 1, to: '/docs/platform/1', name: 'Модуль «Почтовый клиент»' },
 					]
 				},
 				{ id: 7, expand: false, name: 'Приложение «Делопроизводство»',
 					childs: [
-						{ id: 1, to: '', name: 'Приложение «Делопроизводство» v.5.4.4' },
+						{ id: 1, to: '/docs/platform/1', name: 'Приложение «Делопроизводство» v.5.4.4' },
 					] 
 				}, 
 				{ id: 8, expand: false, name: 'Приложение «Управление документами»',
 					childs: [
-						{ id: 1, to: '', name: 'Приложение «Управление документами» Docsvision 5.4.2642' },
-						{ id: 1, to: '', name: 'Приложение «Управление документами» Docsvision 5.3.2559' },
-						{ id: 2, to: '', name: 'Приложение «Управление документами» Docsvision 5.3.2529' },
-						{ id: 3, to: '', name: 'Приложение «Управление документами» Docsvision 5.2.2450' },
+						{ id: 1, to: '/docs/platform/1', name: 'Приложение «Управление документами» Docsvision 5.4.2642' },
+						{ id: 1, to: '/docs/platform/2', name: 'Приложение «Управление документами» Docsvision 5.3.2559' },
+						{ id: 2, to: '/docs/platform/3', name: 'Приложение «Управление документами» Docsvision 5.3.2529' },
+						{ id: 3, to: '/docs/platform/4', name: 'Приложение «Управление документами» Docsvision 5.2.2450' },
 					]
 				},
 			],
