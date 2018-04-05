9b69bd278c17b523289d387a20e719c2840d80a2
Volkov Alexander <volkov.525@gmail.com>
2018-04-05 13:42:08 +0300

    add platform icon

--- components/ProductCarousel.vue
+++ components/ProductCarousel.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
-carousel(:per-page='2' class='carousel')
+carousel(:per-page='3' :loop='true' :navigationEnabled='true' class='carousel')
 	slide(v-for="product in products" :key="product.id" class="slide")
-		.product
-			div {{product.name}}
-			div {{ product.id }}
+		.product-card
+			.product(@click="goto(product.to)")
+				i(:class="product.icon")
+				h3 {{product.name}}
+				<!-- .title {{product.name}} -->
 
 </template>
 
@@ -12,17 +14,20 @@ carousel(:per-page='2' class='carousel')
 		data() {
 			return {
 			products: [
-				{ id: 1, name: 'Платформа', icon: '', text: '' },
+				{ id: 1, name: 'Платформа', icon: 'icon-platform', text: '', to: '/docs/platform/1' },
 				{ id: 2, name: 'Web-client', icon: '', text: '' },
-				{ id: 3, name: 'Платформа', icon: '', text: '' },
-				{ id: 4, name: 'Пульс', icon: '', text: '' },
-				{ id: 5, name: 'Накопительные обновления', icon: '', text: '' },
-				{ id: 6, name: 'Договоры', icon: '', text: '' },
-				{ id: 7, name: 'Почтовый клиент', icon: '', text: '' },
-				{ id: 8, name: 'Делопроизводство', icon: '', text: '' },
-				{ id: 9, name: 'Управление документами', icon: '', text: '' },
+				{ id: 3, name: 'Пульс', icon: '', text: '' },
+				{ id: 4, name: 'Накопительные обновления', icon: '', text: '' },
+				{ id: 5, name: 'Договоры', icon: '', text: '' },
+				{ id: 6, name: 'Почтовый клиент', icon: '', text: '' },
+				{ id: 7, name: 'Делопроизводство', icon: '', text: '' },
+				{ id: 8, name: 'Управление документами', icon: '', text: '' },
 			],
-				
+			}
+		},
+		methods: {
+			goto(e) {
+				this.$router.push(e);
 			}
 		}
 	}
@@ -32,14 +37,29 @@ carousel(:per-page='2' class='carousel')
 .carousel {
 	width: 780px;
 	margin: 0 auto;
+	/* border: 1px solid #ccc; */
 }
 .slide {
-	background: #ccc;
+	/* background: #ccc; */
+}
+.product-card {
+	margin: 0 .5rem;
+	background: #eee;
+	height: 12rem;
+	padding: .5rem;
+
 }
 .product {
-	margin: .5rem;
-	background: #eee;
-
+	background: #fff;
+	height: 100%;
+	border-radius: .5rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	i {
+		font-size: 6rem;
+	}
 }
 
 </style>
