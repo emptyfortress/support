f8893a1cafad5ebe60a27ddef3b551bab69f6040
Volkov Alexander <volkov.525@gmail.com>
2018-04-10 15:43:23 +0300

    added tags

--- pages/product/_id.vue
+++ pages/product/_id.vue
@@ -3,9 +3,12 @@
 	h2 {{current[0].name}}: уточните тему
 	p Выполните поиск или выберите нужную тему из списка ниже.
 	<!-- .navgrid -->
+	br/
 	.tags
-		.tag(v-for="section in current[0].sections" :key="section.id" :icon="section.icon" :name="section.name" :to="section.to") {{section.name}}
-		<!-- .tag test -->
+		.tag.is-medium(v-for="section in current[0].sections" 
+										:class="section.active ? 'is-primary' : ''"
+										@click="section.active = !section.active"
+									) {{section.name}}
 </template>
 
 <script>
@@ -18,14 +21,14 @@ import Section from '@/components/Section';
 					{ 
 						id: '_platform', name: 'Платформа',
 						sections: [
-							{ id: 5, name: 'Карточки', to: '', },
-							{ id: 6, name: 'Справочники', to: '', },
-							{ id: 2, name: 'Решения', to: '', },
-							{ id: 2, name: 'Конструкторы', to: '', },
-							{ id: 1, name: 'Совместимость', to: '', },
-							{ id: 3, name: 'Демоверсии', to: '', },
-							{ id: 4, name: 'Администрирование', to: '', },
-							{ id: 7, name: 'Workflow', to: '', },
+							{ id: 5, active: false, name: 'Карточки', to: '', },
+							{ id: 6, active: false, name: 'Справочники', to: '', },
+							{ id: 2, active: false, name: 'Решения', to: '', },
+							{ id: 2, active: false, name: 'Конструкторы', to: '', },
+							{ id: 1, active: false, name: 'Совместимость', to: '', },
+							{ id: 3, active: false, name: 'Демоверсии', to: '', },
+							{ id: 4, active: false, name: 'Администрирование', to: '', },
+							{ id: 7, active: false, name: 'Workflow', to: '', },
 						]
 					},
 					{ id: '_web', name: 'Web-client', },
@@ -51,28 +54,10 @@ import Section from '@/components/Section';
 .container { margin-top: 4rem; }
 h1 { margin-bottom: 1rem; }
 p { font-size: 1.2rem; }
-.navgrid {
-	/* background: #eee; */
-	max-width: 960px;
-	height: auto;
-	/* margin: 1rem auto; */
-	display: flex;
-	flex-flow: row wrap;
-	align-items: center;
-	/* justify-content: center; */
+.tag {
+	cursor: pointer;
 }
 
-.item {
-	border: 1px solid #fff;
-	width: 220px;
-	margin-bottom: 1rem;
-	margin-right: 1rem;
-	padding: .5rem;
-	text-transform: uppercase;
-	border-radius: 2rem;
-	&:hover {
-		border: 1px solid #ccc;
-	}
-}
+
 
 </style>
