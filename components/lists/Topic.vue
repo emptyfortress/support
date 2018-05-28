dd36974bd3cbfaf7b22a539f600fc8cd0559d232
Volkov Alexander <volkov.525@gmail.com>
2018-05-28 12:08:54 +0300

    finish list of topics

---
+++ components/lists/Topic.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+.myrow.level
+	.level-left
+		i.icon-user.level-item
+		.attr
+			.zag {{ title }}
+			.fio {{ fio }} &bull; 3 мес. назад
+	.level-right
+		.level-item.small.tooltip(data-tooltip="Ответов")
+			i.icon-bubbles {{ reply }}
+		.level-item.small.tooltip(data-tooltip="Проголосовали")
+			i.icon-counter {{ counter }}
+
+</template>
+
+<script>
+	export default {
+		props: ['title', 'fio', 'reply', 'counter']
+		
+	}
+</script>
+
+
+<style scoped lang="scss">
+
+.myrow {
+	border-top: 1px solid #eee;
+	/* border-bottom: 1px solid #eee; */
+	padding: .5rem 0;
+	i.icon-user {
+		font-size: 2.3rem;
+		color: $dv-gray3;
+	}
+	&.level {
+		margin-bottom: 0;
+	}
+}
+.zag {
+	font-size: 1.3rem;
+	color: $dv-blue;
+	cursor: pointer;
+	&:hover {
+		text-decoration: underline;
+	}
+}
+.fio {
+	font-weight: 300;
+	margin-top: .4rem;
+	color: #ccc;
+}
+.small i {
+	font-size: 1rem;
+	margin-left: 1rem;
+}
+</style>
--- pages/forum/_list.vue
+++ pages/forum/_list.vue
@@ -1,88 +1,119 @@
 <template lang="pug">
 .container
 	h2
-		i.icon-bubles/ {{ forums[ indeks ].title }}
-
+		i.icon-forum/ {{ forums[ indeks ].title }}
+	p {{ forums[ indeks ].desc }}
+	br
 	.tabs
 		ul
 			li(v-for='tab in tabs'  :class="activeTab == tab.id ? 'is-active' : ''")
 				a(@click="setTab(tab.id)") {{ tab.name }}
-
 	.tags
 		.tag.is-rounded.is-medium(v-for="section in sections" 
 										:class="section.active ? 'is-info' : ''"
 										@click="section.active = !section.active"
 									) {{section.name}}
 
-
 	#tab1(v-if="activeTab == 1")
-		.level
-			.level-left laks lsk
-			.level-right sdasdasd
+		Topic(v-for='topic in topics' :key="topic.id" :title="topic.title" :fio="topic.fio" :reply="topic.reply" :counter="topic.counter")
 
+	#tab2(v-if="activeTab == 2")
+		Topic(v-for='topic in tab2' :key="topic.id" :title="topic.title" :fio="topic.fio" :reply="topic.reply" :counter="topic.counter")
 
+	#tab3(v-if="activeTab == 3")
+		Topic(v-for='topic in tab3' :key="topic.id" :title="topic.title" :fio="topic.fio" :reply="topic.reply" :counter="topic.counter")
 
-
-	#tab2(v-if="activeTab == 2") tab2
-	#tab3(v-if="activeTab == 3") tab3
-	#tab4(v-if="activeTab == 4") tab4
+	#tab4(v-if="activeTab == 4")
+		Topic(v-for='topic in tab4' :key="topic.id" :title="topic.title" :fio="topic.fio" :reply="topic.reply" :counter="topic.counter")
 
 </template>
 
 <script>
-	export default {
-		transition(to, from) {
-			if (!from) return 'slide-left'
-			return +to.params.id > +from.params.id ? 'slide-right' : 'slide-left'
+import Topic from '@/components/lists/Topic';
+	
+
+export default {
+	transition(to, from) {
+		if (!from) return 'slide-left'
+		return +to.params.id > +from.params.id ? 'slide-right' : 'slide-left'
+	},
+	data() {
+		return {
+		activeTab: 1,
+		tabs: [
+			{ id: 1, name: 'Все' },
+			{ id: 2, name: 'Популярное' },
+			{ id: 3, name: 'Новое' },
+			{ id: 4, name: 'Дискуссии' },
+		],
+		sections: [
+			{ id: 1 , active: true, name: 'Все', to: '', },
+			{ id: 2 , active: false, name: 'Запланировано', to: '', },
+			{ id: 3 , active: false, name: 'Не запланировано', to: '', },
+			{ id: 4 , active: false, name: 'Завершено', to: '', },
+			{ id: 5 , active: false, name: 'С ответом', to: '', },
+			{ id: 6 , active: false, name: 'Без статуса', to: '', },
+		],
+		forums: [
+			{ id: 1, title: "ЧаВо", desc: "Форум всех вопросов"  },
+			{ id: 2, title: "Идеи и предложения", desc: "Предложения по развитию функционала ДВ"  },
+			{ id: 3, title: "Администрирование ДВ", desc: "Обсуждение сбоев"  },
+			{ id: 4, title: "ДВ-информер", desc: "Вопросы по ДВ-информер"  },
+			{ id: 5, title: "Конструкторы", desc: "Конструирование решений на платформе ДВ"  },
+			{ id: 6, title: "Форсаж 5", desc: "Производительность ДВ 5"  },
+			{ id: 7, title: "Демоверсии", desc: "Вопросы Docsvision Live"  },
+			{ id: 8, title: "Обучение", desc: "Основы программирования в решениях ДВ"  },
+			{ id: 9, title: "Проекты", desc: "Вопросы создания СЭД"  },
+		],
+		topics: [
+			{ id: 1, title: "Доминантсептаккорд имеет нечетный канал", fio: "Семен Семенович", reply: 2, counter: 4  },
+			{ id: 2, title: "Септаккорд многопланово иллюстрирует райдер", fio: "Аристарх Сигизмундович", reply: 2, counter: 4  },
+			{ id: 3, title: "Рондо, так или иначе, варьирует голос", fio: "Марфа Степановна", reply: 2, counter: 4  },
+			{ id: 4, title: "Струна, по определению, начинает перекрестный флюгель-горн", fio: "Барак Обамович", reply: 2, counter: 4  },
+			{ id: 5, title: "Арпеджио, так или иначе, иллюстрирует кризис жанра", fio: "Трифон Ильич", reply: 2, counter: 4  },
+			{ id: 6, title: "Канал заканчивает фузз", fio: "Ольга Петровна", reply: 2, counter: 4  },
+			{ id: 7, title: "Трехчастная фактурная форма полифигурно выстраивает полиряд", fio: "Семен Семенович", reply: 2, counter: 4  },
+			{ id: 8, title: "Крещендирующее хождение образует нечетный ревер", fio: "Марфа Степановна", reply: 2, counter: 4  },
+			{ id: 9, title: "Нежилое помещение подведомственно арбитражному суду", fio: "Трифон Ильич", reply: 2, counter: 4  },
+			{ id: 10, title: "Взаимозачет публичен", fio: "Аристарх Сигизмундович", reply: 2, counter: 4  },
+			{ id: 11, title: "Законодательство запрещает акционерный индоссамент", fio: "Марфа Степановна", reply: 2, counter: 4  },
+			{ id: 12, title: "Перестрахование акцептовано", fio: "Петр Петрович", reply: 2, counter: 4  },
+		]
+		}
+	},
+	computed: {
+		indeks() {
+			let mylist = [ 'faq', 'idea', 'admin', 'informer', 'construct', 'efficient', 'live', 'program', 'project' ];
+			return mylist.indexOf( this.$route.params.list );
 		},
-		data() {
-			return {
-			activeTab: 1,
-			tabs: [
-				{ id: 1, name: 'Все' },
-				{ id: 2, name: 'Популярное' },
-				{ id: 3, name: 'Новое' },
-				{ id: 4, name: 'Дискуссии' },
-			],
-			sections: [
-				{ id: 1 , active: true, name: 'Все', to: '', },
-				{ id: 2 , active: false, name: 'Запланировано', to: '', },
-				{ id: 3 , active: false, name: 'Не запланировано', to: '', },
-				{ id: 4 , active: false, name: 'Завершено', to: '', },
-				{ id: 5 , active: false, name: 'С ответом', to: '', },
-				{ id: 6 , active: false, name: 'Без статуса', to: '', },
-
-
-			],
-			forums: [
-				{ id: 1, title: "ЧаВо"  },
-				{ id: 2, title: "Идеи и предложения"  },
-				{ id: 3, title: "Администрирование ДВ"  },
-				{ id: 4, title: "ДВ-информер"  },
-				{ id: 5, title: "Конструкторы"  },
-				{ id: 6, title: "Форсаж 5"  },
-				{ id: 7, title: "Демоверсии"  },
-				{ id: 8, title: "Обучение"  },
-				{ id: 9, title: "Проекты"  },
-			]
+		tab2() {
+			function checkEven(arg) {
+				return arg.id > 5;
 			}
+			return this.topics.filter(checkEven);
 		},
-		computed: {
-			indeks() {
-				let mylist = [ 'faq', 'idea', 'admin', 'informer', 'construct', 'efficient', 'live', 'program', 'project' ];
-				return mylist.indexOf( this.$route.params.list );
+		tab3() {
+			function checkEven(arg) {
+				return arg.id > 8;
 			}
+			return this.topics.filter(checkEven);
 		},
-		methods: {
-			setTab(e) {
-				this.activeTab = e;
-			},
+		tab4() {
+			function checkEven(arg) {
+				return arg.id > 10;
+			}
+			return this.topics.filter(checkEven);
+		}
+	},
+	methods: {
+		setTab(e) {
+			this.activeTab = e;
 		},
-
-
-	// layout: 'main',
-		
+	},
+	components: {
+		Topic
 	}
+}
 </script>
 
 <style scoped lang="scss">
@@ -91,5 +122,4 @@
 .tag {
 	cursor: pointer;
 }
-
 </style>
