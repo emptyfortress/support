5b6a626a78b38803b5f35ad0747580d5a2a9bdff
Volkov Alexander <volkov.525@gmail.com>
2018-06-27 13:09:20 +0300

    add component for branch in tree

--- components/roadCarousel.vue
+++ components/roadCarousel.vue
@@ -8,73 +8,58 @@
 			.product(:class="index == activeVersion ? 'active' : ''" @click="setActiveItem(index)")
 				p.dv Docsvision
 				p {{product.name}}
+	.head
 	.treeContainer
-		.section(v-if="activeVersion == 0")
-			p this is 1
-			p this is 1
-			p this is 1
-			p this is 1
-			p this is 1
-		.section(v-if="activeVersion == 1")
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-			p this is 2
-		.section(v-if="activeVersion == 2")
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
-			p this is 3
+		section(v-if="activeVersion == 0" )
+			div(v-for="item in version53" )
+				Branch(:name="item.name" :date="item.date")
+		section(v-if="activeVersion == 1")
+		section(v-if="activeVersion == 2")
+	.tail
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				activeVersion: 1,
-				products: [
-					{ id: 1, name: '5.3',  active: false, to: '' },
-					{ id: 2, name: '5.4',  active: true, to: ''},
-					{ id: 3, name: '5.5',  active: false, to: ''},
-				],
-			}
-		},
-		computed: {
-		},
-		mounted() {
-			this.overflow();
-			this.setActiveItem(1);
-		},
-		methods: {
-			overflow() { // this is for showing items outside of carousel
-				let inner = document.querySelector('.VueCarousel-wrapper')
-				inner.style.overflow = 'visible';
-			},
-			setActiveItem(index) {
-				let slider = document.querySelector('.VueCarousel-inner');
-				let distance = - ( index * 128 );
-				slider.style.transform = "translate3d(" + distance +"px, 0, 0)";
-				this.activeVersion = index;
-      },
+import Branch from '@/components/Branch';
+	
+export default {
+	data() {
+		return {
+			activeVersion: 0,
+			products: [
+				{ id: 1, name: '5.3',  active: false, to: '' },
+				{ id: 2, name: '5.4',  active: true, to: ''},
+				{ id: 3, name: '5.5',  active: false, to: ''},
+			],
+			version53: [
+				{ id: 0, date: "Раз в 2-3 месяца (по мере накопления багов)", name: "Накопительное обновление", to: "" },
+				{ id: 1, date: "Конец 2019 года", name: "Поддержка прекращена", to: "" },
+			]
 		}
+	},
+	computed: {
+	},
+	mounted() {
+		this.overflow();
+		this.setActiveItem(0);
+	},
+	methods: {
+		overflow() { // this is for showing items outside of carousel
+			let inner = document.querySelector('.VueCarousel-wrapper')
+			inner.style.overflow = 'visible';
+		},
+		setActiveItem(index) {
+			let slider = document.querySelector('.VueCarousel-inner');
+			let distance = - ( index * 128 );
+			slider.style.transform = "translate3d(" + distance +"px, 0, 0)";
+			this.activeVersion = index;
+		},
+	},
+	components: {
+		Branch: Branch
 	}
+}
+
+
 </script>
 
 <style scoped lang="scss">
@@ -116,11 +101,20 @@
 	width: 600px;
 	margin: 0 auto;
 	background: url(~assets/img/vert.png) repeat-y center top;
-	.section {
-		width: 100%;
-		/* height: 500px; */
-		/* background: #ccc; */
-	}
+	border: 1px solid red; 
+}
+
+.tail {
+	width: 10px;
+	height: 45px;
+	margin: 0 auto;
+	background: url(~assets/img/tail.png) no-repeat center top;
+}
+.head {
+	width: 10px;
+	height: 45px;
+	margin: 0 auto;
+	background: url(~assets/img/head.png) no-repeat center top;
 }
 
 </style>
