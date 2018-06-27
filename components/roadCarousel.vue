676a355b7956bcd6241ed769e9098fcf5eb2228c
Volkov Alexander <volkov.525@gmail.com>
2018-06-27 09:31:01 +0300

    make roadCarousel work on click and gentle drag

--- components/roadCarousel.vue
+++ components/roadCarousel.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 .inner
-	carousel(:per-page='3' :scrollPerPage='false'
+	carousel(:per-page='1' :scrollPerPage='true'
 					:paginationEnabled='false'
 					:loop='false'
 					class='carousel' )
 		slide(v-for="( product, index ) in products" :key="index"  )
-			.product(:class="product.active ? 'active' : ''" @click="setActiveItem(index)")
+			.product(:class="index == activeVersion ? 'active' : ''" @click="setActiveItem(index)")
 				p.dv Docsvision
 				p {{product.name}}
 </template>
@@ -14,29 +14,35 @@
 	export default {
 		data() {
 			return {
-				activeVersion: 2,
+				activeVersion: 1,
 				products: [
-					{ id: 5, name: '',  active: false, to: '' },
 					{ id: 1, name: '5.3',  active: false, to: '' },
 					{ id: 2, name: '5.4',  active: true, to: ''},
 					{ id: 3, name: '5.5',  active: false, to: ''},
-					{ id: 6, name: '',  active: false, to: ''},
 				],
 			}
 		},
+		computed: {
+		},
 		mounted() {
 			this.startShift();
+			this.overflow();
 		},
 		methods: {
+			overflow() { // this is for showing items outside of carousel
+				let inner = document.querySelector('.VueCarousel-wrapper')
+				inner.style.overflow = 'visible';
+
+			},
 			startShift() {
 				let slider = document.querySelector('.VueCarousel-inner');
-				slider.style.transform = "translate3d("  + slider.style.flexBasis + ", 0, 0)";
-				// slider.style.transform = "translate3d(200px, 0, 0)";
+				slider.style.transform = "translate3d(-128px, 0, 0)";
 			},
 			setActiveItem(index) {
 				let slider = document.querySelector('.VueCarousel-inner');
-				// slider.style.transform = "translate3d(" + flex-basis +"px, 0, 0)";
-				console.log(index);
+				let distance = - ( index * 128 );
+				slider.style.transform = "translate3d(" + distance +"px, 0, 0)";
+				this.activeVersion = index;
       },
 		}
 	}
@@ -45,30 +51,31 @@
 <style scoped lang="scss">
 .inner {
 	margin: 0 auto;
-	border: 1px solid #ccc; 
+	/* border: 1px solid #ccc;  */
 }
 .carousel {
-	width: 400px;
+	width: 130px;
 	margin: 0 auto;
-	border: 1px solid red; 
+	/* border: 1px solid red;  */
 	overflow: visible;
 }
-.VueCarousel-wrapper {
+.carousel .VueCarousel-wrapper {
 	overflow: visible !important;
 }
 .VueCarouser-inner {
 }
 
 .product {
-	background: #eee;
 	margin: 0 .5rem;
 	/* width: 150px; */
 	/* flex-basis: content; */
 	cursor: pointer;
 	padding: .5rem;
 	color: $main;
+	transition: all .3s ease;
 	&.active {
 		color: $dv-green;
+		background: #eee;
 	}
 	i {
 		font-size: 5rem;
