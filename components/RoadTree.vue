24a59ecb72a9437ec3762cd75b940489512ed9b1
Volkov Alexander <volkov.525@gmail.com>
2018-07-05 15:57:00 +0300

    move code to component roadItem for reuse in products

--- components/RoadTree.vue
+++ components/RoadTree.vue
@@ -1,48 +1,54 @@
 <template lang="pug">
 .pri
 	.head
-	ul.treeContainer
-		transition-group(name="fade1" mode="in-out" tag="li")
-			li.branchlist(v-if="ver0" v-for="item in version53" :key="item.id")
-				.section(@click="goTo(to)")
-					.left
-						p {{ item.date }}
-					.center
-					.right
-						p {{ item.name }}
-			li.branchlist(v-if="ver1" v-for="item in version54" :key="item.id")
-				.section(@click="goTo(to)")
-					.left
-						p {{ item.date }}
-					.center
-					.right
-						p {{ item.name }}
-			li.branchlist(v-if="ver2" v-for="item in version55" :key="item.id")
-				.section(@click="goTo(to)")
-					.left
-						p {{ item.date }}
-					.center
-					.right
-						p {{ item.name }}
+	.treeContainer
+		roadItem(:listFeatures="version53" v-if="ver0")
+		roadItem(:listFeatures="version54" v-if="ver1")
+		
+		<!-- transition&#45;group(name="fade1" mode="in&#45;out" tag="li") -->
+		<!-- 	li.branchlist(v&#45;if="ver0" v&#45;for="item in version53" :key="item.id") -->
+		<!-- 		.section(@click="") -->
+		<!-- 			.left -->
+		<!-- 				p {{ item.date }} -->
+		<!-- 			.center -->
+		<!-- 			.right -->
+		<!-- 				p {{ item.name }} -->
+		<!-- 	li.branchlist(v&#45;if="ver1" v&#45;for="item in version54" :key="item.id") -->
+		<!-- 		.section(@click="") -->
+		<!-- 			.left -->
+		<!-- 				p {{ item.date }} -->
+		<!-- 			.center -->
+		<!-- 			.right -->
+		<!-- 				p {{ item.name }} -->
+		<!-- 	li.branchlist(v&#45;if="ver2" v&#45;for="item in version55" :key="item.id") -->
+		<!-- 		.section(@click="") -->
+		<!-- 			.left -->
+		<!-- 				p {{ item.date }} -->
+		<!-- 			.center -->
+		<!-- 			.right -->
+		<!-- 				p {{ item.name }} -->
+
 	.tail
 </template>
 
 <script>
+import roadItem from "@/components/lists/roadItem";
+
 	export default {
 		data() {
 			return {
 				version53: [
-					{ id: 0, date: "Раз в 2-3 месяца (по мере накопления багов)", name: "Накопительное обновление", to: "" },
-					{ id: 1, date: "Конец 2019 года", name: "Прекращение поддержки", to: "" },
+					{ id: 0, date: "Раз в 2-3 месяца (по мере накопления багов)", name: "Накопительное обновление",  },
+					{ id: 1, date: "Конец 2019 года", name: "Прекращение поддержки",  },
 				],
 				version54: [
-					{ id: 0, date: "Осень 2018", name: "Конструктор согласований", to: "" },
-					{ id: 1, date: "Сентябрь 2018", name: "Увеличение быстродействия папок", to: "" },
-					{ id: 2, date: "Октябрь 2018", name: "Фильтры в представлениях", to: "" },
+					{ id: 0, date: "Осень 2018", name: "Конструктор согласований",  },
+					{ id: 1, date: "Сентябрь 2018", name: "Увеличение быстродействия папок",  },
+					{ id: 2, date: "Октябрь 2018", name: "Фильтры в представлениях",  },
 				],
 				version55: [
-					{ id: 0, date: "Раз в 2-3 месяца (по мере накопления багов)", name: "Накопительное обновление", to: "" },
-					{ id: 1, date: "Конец 2019 года", name: "Прекращение поддержки", to: "" },
+					{ id: 0, date: "Раз в 2-3 месяца (по мере накопления багов)", name: "Накопительное обновление",  },
+					{ id: 1, date: "Конец 2019 года", name: "Прекращение поддержки",  },
 				],
 			}
 		},
@@ -51,6 +57,9 @@
 			ver1() { return this.$store.state.activeVersion == 1 ? true : false },
 			ver2() { return this.$store.state.activeVersion == 2 ? true : false },
 		},
+		components: {
+			roadItem
+		}
 	}
 
 </script>
@@ -81,49 +90,9 @@
 	background: url(~assets/img/head.png) no-repeat center top;
 }
 
-.section {
-	width: 100%;
-	display: flex;
-	justify-content: center;
-	.center {
-		width: 15px;
-		height: 15px;
-		border-radius: 15px;
-		border: 3px solid #ccc;
-		background: #fff;
-		transition: all .2s ease;
-		cursor: pointer;
-		&:hover {
-			width: 25px;
-			height: 25px;
-			border-color: $blue;
-		}
-	}
-	.left, .right {
-		/* width: calc(100% - 40px); */
-		width: 30%;
-		background: #eee;
-	}
-	.left {
-		text-align: right; 
-		margin-right: 2rem;
-	}
-	.right {
-		text-align: left; 
-		margin-left: 2rem;
-	}
-}
 
 .pri {
 	transition: .4s all ease;
 }
 
-.fade1-enter-active, .fade1-leave-active {
-  transition: all .5s;
-}
-.fade1-enter, .fade1-leave-to /* .fade1-leave-active below version 2.1.8 */ {
-  opacity: 0;
-	height: 0;
-	transform: translateY(30px);
-}
 </style>
---
+++ components/lists/roadItem.vue
@@ -0,0 +1,78 @@
+<template lang="pug">
+ul
+	transition-group(name="fade1" mode="in-out" tag="li")
+		li.branchlist(v-for="item in listFeatures" :key="item.id")
+			.section(@click="")
+				.left
+					p {{ item.date }}
+				.center
+				.right
+					p {{ item.name }}
+</template>
+
+<script>
+export default {
+	props: ['listFeatures'],
+	data() {
+		return {
+			
+		}
+	},
+	computed: {
+	},
+	methods: {
+	}
+}
+</script>
+
+<style scoped lang="scss">
+.branchlist {
+	margin-bottom: 1rem;
+	&:last-child {
+		margin-bottom: 0;
+	}
+}
+
+.section {
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	.center {
+		width: 15px;
+		height: 15px;
+		border-radius: 15px;
+		border: 3px solid #ccc;
+		background: #fff;
+		transition: all .2s ease;
+		cursor: pointer;
+		&:hover {
+			width: 25px;
+			height: 25px;
+			border-color: $blue;
+		}
+	}
+	.left, .right {
+		/* width: calc(100% - 40px); */
+		width: 30%;
+		background: #eee;
+	}
+	.left {
+		text-align: right; 
+		margin-right: 2rem;
+	}
+	.right {
+		text-align: left; 
+		margin-left: 2rem;
+	}
+}
+
+.fade1-enter-active, .fade1-leave-active {
+  transition: all .5s;
+}
+.fade1-enter, .fade1-leave-to /* .fade1-leave-active below version 2.1.8 */ {
+  opacity: 0;
+	height: 0;
+	transform: translateY(30px);
+}
+
+</style>
