13229db94ebad268a2476ceb3f42946ec5cb1638
Volkov Alexander <volkov.525@gmail.com>
2018-07-06 17:47:03 +0300

    Some small fixes. Not too important

--- components/reqTable.vue
+++ components/reqTable.vue
@@ -11,7 +11,7 @@
 			i.icon-filter(v-if="query.length == 0")
 			i.icon-close(v-else @click="clearQuery")
 	table.table.is-fullwidth.is-hoverable
-		thead
+		thead(@click='sortTable')
 			tr
 				th(v-for="column in columns" :class="column.class") {{ column.name }}
 		transition-group(name="request" tag="tbody")
@@ -20,6 +20,18 @@
 				td {{ row.content }}
 				td {{ row.num }}
 				td {{ row.partners }}
+	hr
+	br
+	nav.pagination.is-rounded
+		a.pagination-previous Previous
+		a.pagination-next Next
+		ul.pagination-list
+			li
+				a.pagination-link.is-current 1
+			li
+				a.pagination-link 2
+			li
+				a.pagination-link 3
 </template>
 
 <script>
@@ -34,15 +46,15 @@ export default {
 			],
 			columns: [
 				{ name: 'Рейтинг', class: '' },
-				{ name: 'Название', class: '' },
+				{ name: 'Название', class: 'has-text-weight-bold' },
 				{ name: 'Номер', class: '' },
-				{ name: 'Партнеры', class: 'has-text-weight-bold' },
+				{ name: 'Партнеры', class: '' },
 			],
 			filteredTable: [
 				{ id:  0, group: 0, start: '', end: '', num: 'BL-201', rating: 21802, tag: '', partners: '', content: 'Использование маски в атрибутивном поиске', },
 				{ id:  1, group: 0, start: '', end: '', num: 'BL-202', rating: 6298 , tag: '', partners: '', content: 'Интеграция с адресными системами (КЛАДР)', },
 				{ id:  2, group: 0, start: '', end: '', num: 'BL-203', rating: 8115 , tag: '', partners: '', content: 'Название вида - локализация, отображаемое название и комментарии', },
-				{ id:  3, group: 0, start: '', end: '', num: 'BL-204', rating: 9932 , tag: 'my', partners: '', content: 'Настройа момента проверки обязательности полей', },
+				{ id:  3, group: 0, start: '', end: '', num: 'BL-204', rating: 9932 , tag: 'my', partners: '', content: 'Настройка момента проверки обязательности полей', },
 				{ id:  4, group: 0, start: '', end: '', num: 'BL-205', rating: 11750, tag: 'my', partners: '', content: 'Настройка условий нумератора - И, ИЛИ', },
 				{ id:  5, group: 0, start: '', end: '', num: 'BL-206', rating: 13567, tag: '', partners: '', content: 'Настройка прав на отдельные нумераторы', },
 				{ id:  6, group: 0, start: '', end: '', num: 'BL-207', rating: 15384, tag: '', partners: '', content: 'Создание Заданий и ГЗ по документам. Шаблоны', },
@@ -60,7 +72,10 @@ export default {
 		},
 		clearQuery() {
 			this.query = '';
-		}
+		},
+		sortTable(e) {
+			this.filteredTable.reverse();
+		},
 
 	}
 }
@@ -105,4 +120,37 @@ td {
 	color: $dv-blue;
 }
 
+nav a {
+	text-decoration: none;
+}
+.pagination-link {
+	&.is-current {
+		background: $blue;
+		border-color: $blue;
+	}
+}
+
+// ========================================
+// animation
+// ========================================
+
+.request-move {
+  transition: transform .6s;
+}
+
+.request-enter-active, .request-leave-active {
+  transition: all .6s;
+}
+.request-leave-active {
+	position: absolute;
+}
+.request-enter, .request-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+.request {
+  backface-visibility: hidden;
+  z-index: 1;
+}
 </style>
